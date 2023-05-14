e6c86780a3c630c0783863e832684f18853cf05d
IskenT <turanov2901@gmail.com>
2023-05-14 16:41:18 +0900

    Variables

---
+++ 03-variables/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var welcome string = "Hello Isken!"   
+	//welcome := "Hello World!"
+	fmt.Println(welcome)
+	var myAge int = 37
+	//myAge := 37
+	myAge++
+	myAge += 10
+	fmt.Println(myAge)
+}
---
+++ 04-const/main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	const welcome string = "Hello const"
+	//const pi float32 = 3.14
+	const pi = 3.14
+	var size float64 = 1.0
+	println(welcome)
+	println(pi*size)
+	println(pi)
+}
