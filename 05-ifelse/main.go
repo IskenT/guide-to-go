e815ec75c58aec401e8980e2528e26ab10053530
IskenT <turanov2901@gmail.com>
2023-05-14 17:01:23 +0900

    if/switch

---
+++ 05-ifelse/main.go
@@ -0,0 +1,11 @@
+package main
+func main(){
+	println("If/else")
+	var userHeight int = 150
+	customerAge := 4
+	if userHeight >= 140 && customerAge <=3{
+		println("Can enter")
+	} else {
+		println("Cannot")
+	}
+}
